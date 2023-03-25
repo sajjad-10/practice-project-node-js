ccdb44903b353b442392f49da0535c993a8d9d08
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-03-25 07:53:26 +0330

    global config

--- project-one/server.js
+++ project-one/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 
+global.config = require("./config");
+
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     res.send("Hello Node");
 });
 
-app.listen(3000, () => {
-    console.log("server is rerunning on port 3000");
+app.listen(config.port, () => {
+    console.log(`server is rerunning on port ${config.port}`);
 });
