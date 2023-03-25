81426b8da6a94a44d0a9d671fc5efc863299499d
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-03-25 08:11:14 +0330

    send data white url  -query--params-

--- project-one/server.js
+++ project-one/server.js
@@ -5,9 +5,15 @@ global.config = require("./config");
 
 app.use(express.static(__dirname + "/public"));
 
+// send data white url -query-
 app.get("/", (req, res) => {
-    res.send("Hello Node");
-});
+    res.send(`Hello Node :${req.query.username}`);
+}); // in url: /?id=10&username=sajjad
+
+// send data white url -params-
+app.get("/:username", (req, res) => {
+    res.send(`Hello Node :${req.params.username}`);
+}); // in url: /:sajjad
 
 app.listen(config.port, () => {
     console.log(`server is rerunning on port ${config.port}`);
