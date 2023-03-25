d0b5ceee48e111018ddcaaebce1c78ed18755c41
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-03-25 14:52:45 +0330

    intro to view engine

--- project-one/server.js
+++ project-one/server.js
@@ -5,7 +5,11 @@ global.config = require("./config");
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+    res.render("index");
+});
 app.use("/user", require("./routes/user"));
 
 app.listen(config.port, () => {
