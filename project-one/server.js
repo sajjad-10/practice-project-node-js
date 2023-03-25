c0cab1b99d66e7a1e8c4b4cee102c7abc0479b99
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-03-25 09:32:08 +0330

    restful api

--- project-one/server.js
+++ project-one/server.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const app = express();
+let users = require("./users");
 
 global.config = require("./config");
 
 app.use(express.static(__dirname + "/public"));
 
-// send data white url -query-
 app.get("/", (req, res) => {
-    res.send(`Hello Node :${req.query.username}`);
-}); // in url: /?id=10&username=sajjad
+    res.status(200).json({
+        data: users,
+        success: true,
+    });
+}); // see all user
 
-// send data white url -params-
-app.get("/:username", (req, res) => {
-    res.send(`Hello Node :${req.params.username}`);
-}); // in url: /:sajjad
+app.get("/:id", (req, res) => {
+    let user = users.filter((user) => {
+        if (user.id == req.params.id) {
+            return user;
+        }
+    });
+    res.status(200).json({
+        data: user,
+        success: true,
+    });
+}); // see one user
 
 app.listen(config.port, () => {
     console.log(`server is rerunning on port ${config.port}`);
