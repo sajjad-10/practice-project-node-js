05f483b0fe755c48503a83a25477d8e8c1646219
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-14 13:33:52 +0330

    routing req

--- node_crash_app/app.js
+++ node_crash_app/app.js
@@ -1,10 +1,24 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type','text/html')
+    const url = req.url;
 
-    res.write("<h1>Hello Node</h1>");
-    res.end
+    if (url === "/") {
+        res.setHeader("Content-Type", "text/html");
+        const form = `
+            <form method="POST" action="/products">
+                <input type="text" name="product" />
+                <button type="submit" name="product">Add Product</button>
+            </form>
+        `;
+        res.write(form);
+        return res.end;
+    }
+
+    res.setHeader("Content-Type", "text/html");
+    const title = `<h2>Shop Page</h2>`;
+    res.write(title);
+    res.end();
 });
 
 server.listen(8001);
