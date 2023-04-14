39c6f8c4ab36b27620e4375173d412dbd5b7829e
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-14 21:54:59 +0330

    express middleware

--- node_crash_app/app.js
+++ node_crash_app/app.js
@@ -1,1 +1,16 @@
-console.log("Node");
+// const http = require("http");
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("First Middleware");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("Second Middleware");
+});
+
+// const server = http.createServer(app);
+// server.listen(8000);
+app.listen(8000); // comment code blow for create a server but in express jus use --> app.listen(8000);
