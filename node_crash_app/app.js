f002b04721ecbde862a75a51d4aeee141bf0c841
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-14 07:27:58 +0330

    sending responses

--- node_crash_app/app.js
+++ node_crash_app/app.js
@@ -1,7 +1,10 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type','text/html')
+
+    res.write("<h1>Hello Node</h1>");
+    res.end
 });
 
-server.listen(8000);
+server.listen(8001);
