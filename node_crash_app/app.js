06cd412f70d6fea076df79f237de5c0e016f5ee8
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-15 09:56:31 +0330

    parsing requests

--- node_crash_app/app.js
+++ node_crash_app/app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
+const form = `
+    <form action="/product" method="POST">
+        <input type="text" name="title"/>
+        <button>Add Product</button>
+    </form>
+`;
+
 app.use("/add-product", (req, res, next) => {
-    res.send("<h2>Add Product</h2>");
+    res.send(form);
+});
+
+app.use("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
