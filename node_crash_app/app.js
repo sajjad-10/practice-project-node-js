6bf29aaa0ef25270eae0b29ee85317ed1434efc6
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-16 12:26:10 +0330

    get & post requests

--- node_crash_app/app.js
+++ node_crash_app/app.js
@@ -14,7 +14,7 @@ app.use("/add-product", (req, res, next) => {
     res.send(form);
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
     console.log(req.body);
     res.redirect("/");
 });
