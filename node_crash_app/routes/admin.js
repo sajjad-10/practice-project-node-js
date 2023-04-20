a06c25de93f7b14a52085bfda1ecb182997ab4c3
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-20 07:14:59 +0330

    add & serving html page

--- node_crash_app/routes/admin.js
+++ node_crash_app/routes/admin.js
@@ -1,15 +1,11 @@
+const path = require("path");
+
 const express = require("express");
 const router = express.Router();
 
-const form = `
-    <form action="/product" method="POST">
-        <input type="text" name="title"/>
-        <button>Add Product</button>
-    </form>
-`;
-
 router.get("/add-product", (req, res, next) => {
-    res.send(form);
+    // res.send(form);
+    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
--- node_crash_app/routes/shop.js
+++ node_crash_app/routes/shop.js
@@ -1,9 +1,12 @@
+const path = require("path");
+
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    console.log("Second Middleware");
-    res.send("<h2>Product List</h2>");
+    // console.log("Second Middleware");
+    // res.send("<h2>Product List</h2>");
+    res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
