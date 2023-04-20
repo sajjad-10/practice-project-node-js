a732e55c1cc3b73b03e7f9d9c03e00df3f2c0c0b
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-20 05:19:14 +0330

    adding 404 page

--- node_crash_app/app.js
+++ node_crash_app/app.js
@@ -9,4 +9,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+// app.use('*',(req, res, next) => {
+//     res.send("<h2>404 | Not Found</h2>");
+// });
+
+app.use((req, res, next) => {
+    res.status(404).send("<h2>404 | Not Found</h2>");
+});
+
 app.listen(8000);
