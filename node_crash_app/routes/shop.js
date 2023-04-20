d2a2e0b321c9c31fd3b7f772d1c3a8e4c01a2990
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-20 20:12:00 +0330

    dynamic content

--- node_crash_app/routes/shop.js
+++ node_crash_app/routes/shop.js
@@ -6,9 +6,10 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+    const products = adminData.products;
     console.log("shop", adminData.products);
     // res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
-    res.render("shop", { pageTitle: "Shop" });
+    res.render("shop", { pageTitle: "Shop", products: products });
 });
 
 module.exports = router;
