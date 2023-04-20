b7f998932d8b80169f702efb87f9b820ef68f6a1
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-20 07:41:52 +0330

    add styles

--- node_crash_app/app.js
+++ node_crash_app/app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const app = express();
 
@@ -5,8 +7,9 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin',adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 // app.use('*',(req, res, next) => {
