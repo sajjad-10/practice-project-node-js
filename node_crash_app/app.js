6b46e9b070627cfe10e84d3bb3a135241322b544
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-20 15:32:07 +0330

    adding ejs

--- node_crash_app/app.js
+++ node_crash_app/app.js
@@ -3,6 +3,9 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
