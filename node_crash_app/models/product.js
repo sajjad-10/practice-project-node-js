9e57f48acdf2199dc87450c1a6da41cc985cf493
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-21 14:51:27 +0330

    storing data in file

--- node_crash_app/models/product.js
+++ node_crash_app/models/product.js
@@ -1,11 +1,27 @@
-const products = [];
-
+const fs = require("fs");
+const path = require("path");
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
     save() {
-        products.push(this);
+        const filePath = path.join(
+            path.dirname(require.main.filename),
+            "data",
+            "products.json"
+        );
+        fs.readFile(filePath, (err, data) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(data);
+            }
+
+            products.push(this);
+
+            fs.writeFile(filePath, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
     static fetchAll() {
         return products;
