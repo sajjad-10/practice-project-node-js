0bb88f9d7c2bcf2f30b74c7098c4db8936157caf
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-21 10:24:11 +0330

    mvc model

--- node_crash_app/controllers/products.js
+++ node_crash_app/controllers/products.js
@@ -1,15 +1,16 @@
-const products = [];
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
     res.render("add-product", { pageTitle: "Add Product" });
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const { title } = req.body;
-    products.push({ title: title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll();
     res.render("shop", { pageTitle: "Shop", products: products });
 };
