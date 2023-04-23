375c03591b8ebdeba21de7f512c22806ed006c94
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-23 13:18:07 +0330

    model in mongodb

--- node_crash_app/models/product.js
+++ node_crash_app/models/product.js
@@ -1,41 +1,10 @@
-const fs = require("fs");
-const path = require("path");
-module.exports = class Product {
-    constructor(title) {
+const mongoConnect = require("../util/database").getDB;
+class Product {
+    constructor(title, price, description) {
         this.title = title;
+        this.price = price;
+        this.description = description;
     }
-    save() {
-        const filePath = path.join(
-            path.dirname(require.main.filename),
-            "data",
-            "products.json"
-        );
-        fs.readFile(filePath, (err, data) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(data);
-            }
+}
 
-            products.push(this);
-
-            fs.writeFile(filePath, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
-        });
-    }
-    static fetchAll(callback) {
-        const filePath = path.join(
-            path.dirname(require.main.filename),
-            "data",
-            "products.json"
-        );
-
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                callback([]);
-            }
-
-            callback(JSON.parse(data));
-        });
-    }
-};
+module.exports = Product;
