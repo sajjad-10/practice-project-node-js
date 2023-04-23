27add8451d75c10bebb0ed597693f583cec1b8ae
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-23 13:07:22 +0330

    optimize connection

--- node_crash_app/app.js
+++ node_crash_app/app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-const mongoConnect = require("./util/database");
+const mongoConnect = require("./util/database").mongoConnect;
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -25,7 +25,6 @@ app.use((req, res, next) => {
     res.status(404).send("<h2>404 | Not Found</h2>");
 });
 
-mongoConnect((client) => {
-    console.log(client);
+mongoConnect(() => {
     app.listen(8000);
 });
--- node_crash_app/util/database.js
+++ node_crash_app/util/database.js
@@ -2,13 +2,27 @@ const { MongoClient } = require("mongodb");
 
 const uri = "mongodb://127.0.0.1:27017/node_crash_app";
 
+let db;
+
 const mongoConnect = (callback) => {
     MongoClient.connect(uri)
-        .then((result) => {
+        .then((client) => {
             console.log("Connected");
-            callback(result);
+            db = client.db();
+            callback();
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+            console.log(err);
+            throw err;
+        });
 };
 
-module.exports = mongoConnect;
+const getDB = () => {
+    if (db) {
+        return db;
+    }
+    throw "No DB Found";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
