4fd801fceebbb283628a7b15bae84703632b6dc2
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-23 21:56:41 +0330

    save documents in db

--- blog_api/controllers/posts-controllers.js
+++ blog_api/controllers/posts-controllers.js
@@ -1,37 +1,34 @@
 const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 
-let posts = [
-    { id: "p1", title: "Title", content: "Content" },
-    { id: "p2", title: "Title", content: "Content" },
-];
+const Post = require("../models/posts");
 
 const getPostById = (req, res, next) => {
-    const postId = req.params.pid;
-    const post = posts.find((item) => {
-        return item.id === postId;
-    });
-    res.json({ post: post });
+    // const postId = req.params.pid;
+    // const post = posts.find((item) => {
+    //     return item.id === postId;
+    // });
+    // res.json({ post: post });
 };
 
-const createPosts = (req, res, next) => {
+const createPosts = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         res.status(402).json({ message: "Invalid data." });
     }
 
     const { title, content } = req.body;
-    const createdPost = { id: uuidv4(), title: title, content: content };
-    posts.push(createdPost);
+    const createdPost = new Post({ title, content });
+    await createdPost.save();
     res.status(201).json({ post: createdPost });
 };
 
 const deletePost = (req, res, next) => {
-    const postId = req.params.pid;
-    posts.filter((item) => {
-        return item.id !== postId;
-    });
-    res.status(200).json({ message: "Post Deleted" });
+    // const postId = req.params.pid;
+    // posts.filter((item) => {
+    //     return item.id !== postId;
+    // });
+    // res.status(200).json({ message: "Post Deleted" });
 };
 
 exports.getPostById = getPostById;
