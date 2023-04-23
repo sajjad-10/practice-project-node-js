09609451ab4f39cd1377797be3a6584a44f30a68
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-23 20:50:46 +0330

    user controllers

--- blog_api/controllers/users-controllers.js
+++ blog_api/controllers/users-controllers.js
@@ -1,6 +1,27 @@
-const getUsers = (req, res, next) => {};
-const singUp = (req, res, next) => {};
-const login = (req, res, next) => {};
+const { v4: uuidv4 } = require("uuid");
+
+const users = [{ id: "u1", email: "test@gmail.com", password: "test" }];
+
+const getUsers = (req, res, next) => {
+    res.json({ users: users });
+};
+const singUp = (req, res, next) => {
+    const { email, password } = req.body;
+    const newUser = { id: uuidv4(), email, password };
+    users.push(newUser);
+    res.status(201).json({ user: newUser });
+};
+const login = (req, res, next) => {
+    const { email, password } = req.body;
+    const validUser = users.find((item) => {
+        return item.email === email;
+    });
+
+    if (!validUser || validUser.password != password) {
+        res.json({ massage: "User not valid." });
+    }
+    res.json({ massage: "Logged in." });
+};
 
 exports.getUsers = getUsers;
 exports.singUp = singUp;
