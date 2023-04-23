be1c6cf5738d078e8d9952d091d073b1bea2302f
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-23 17:49:29 +0330

    Filter routes

--- blog_api/app.js
+++ blog_api/app.js
@@ -4,6 +4,6 @@ const postRoutes = require("./routes/posts-routes");
 
 const app = express();
 
-app.use(postRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(5000);
--- blog_api/routes/posts-routes.js
+++ blog_api/routes/posts-routes.js
@@ -2,8 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    res.json({ message: "post Route" });
+const posts = [
+    { id: "p1", title: "Title", content: "Content" },
+    { id: "p2", title: "Title", content: "Content" },
+];
+
+router.get("/:pid", (req, res, next) => {
+    const postId = req.params.pid;
+    const post = posts.find((item) => {
+        return item.id === postId;
+    });
+    res.json({ post: post });
 });
 
 module.exports = router;
