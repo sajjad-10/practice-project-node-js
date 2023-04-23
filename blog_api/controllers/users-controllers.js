1992d94b250b786207dc10726e4ea56aaf49febe
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-23 22:16:07 +0330

    user signUp

--- blog_api/controllers/users-controllers.js
+++ blog_api/controllers/users-controllers.js
@@ -1,26 +1,25 @@
 const { v4: uuidv4 } = require("uuid");
 
-const users = [{ id: "u1", email: "test@gmail.com", password: "test" }];
+const User = require("../models/users");
 
 const getUsers = (req, res, next) => {
-    res.json({ users: users });
+    // res.json({ users: users });
 };
-const singUp = (req, res, next) => {
+const singUp = async (req, res, next) => {
     const { email, password } = req.body;
-    const newUser = { id: uuidv4(), email, password };
-    users.push(newUser);
+    const newUser = new User({ email, password });
+    await newUser.save();
     res.status(201).json({ user: newUser });
 };
 const login = (req, res, next) => {
-    const { email, password } = req.body;
-    const validUser = users.find((item) => {
-        return item.email === email;
-    });
-
-    if (!validUser || validUser.password != password) {
-        res.json({ massage: "User not valid." });
-    }
-    res.json({ massage: "Logged in." });
+    // const { email, password } = req.body;
+    // const validUser = users.find((item) => {
+    //     return item.email === email;
+    // });
+    // if (!validUser || validUser.password != password) {
+    //     res.json({ massage: "User not valid." });
+    // }
+    // res.json({ massage: "Logged in." });
 };
 
 exports.getUsers = getUsers;
